1b71c04e3bff083e772bf7b64c75e07771522f68
repeater <none@none>
2017-12-10 14:54:04 +0200

    add allocator for working with virtual memory

--- core/std/allocator.h
+++ core/std/allocator.h
@@ -16,6 +16,14 @@ struct aligned_mem_allocator
 	inline void deallocate( pointer p );
 };
 
+struct virtual_mem_allocator
+{
+	inline pointer reserve( pointer p, uptr size );
+	inline pointer commit( pointer p, uptr size );
+	inline void decommit( pointer p, uptr size );
+	inline void release( pointer p, uptr size );
+};
+
 #include "allocator_inline.h"
 
 #endif // #ifndef __core_allocator_h_included_
