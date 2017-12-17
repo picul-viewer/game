13f60f15a8e60a821840ff64a3bcb69c596c6506
repeater <none@none>
2017-12-17 14:39:25 +0200

    add threading event class

---
+++ core/threads/threading_event.h
@@ -0,0 +1,27 @@
+#ifndef __core_threading_event_h_included_
+#define __core_threading_event_h_included_
+
+#include <core/types.h>
+#include <Windows.h>
+
+class threading_event
+{
+public:
+	threading_event( ) = default;
+	~threading_event( ) = default;
+
+	threading_event( bool initial_state, bool use_manual_reset = true );
+
+	void create( bool initial_state = true, bool use_manual_reset = true );
+	void destroy( );
+
+	void set( );
+	void reset( );
+
+	void wait_for( u32 wait_ms = INFINITE );
+
+protected:
+	HANDLE	m_id;
+};
+
+#endif // #ifndef __core_threading_event_h_included_
