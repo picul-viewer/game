4615f125aea1b5385d4d3a5475d2c2fde76d91c1
Picul <me@example.com>
2017-12-29 21:33:03 +0200

    rework constant buffers ( add untyped constant buffer )
    --HG--
    branch : render v0

--- render/render/constant_buffer_inline.h
+++ render/render/constant_buffer_inline.h
@@ -6,59 +6,15 @@
 namespace render {
 
 template<typename buffer_struct>
-void constant_buffer<buffer_struct>::create( buffer_struct* in_data, bool in_is_dynamic )
+void typed_constant_buffer<buffer_struct>::create( buffer_struct* in_data, bool in_is_dynamic )
 {
-	buffer::cook buffer_cook;
-	buffer_cook.set_constant_buffer( sizeof(buffer_struct), in_is_dynamic );
-	buffer::create( buffer_cook, in_data );
+	create( in_data, sizeof(buffer_struct), in_is_dynamic );
 }
 
 template<typename buffer_struct>
-void constant_buffer<buffer_struct>::update( buffer_struct const& in_data ) const
+void typed_constant_buffer<buffer_struct>::update( buffer_struct const& in_data ) const
 {
-	buffer::update_dynamic( &in_data, 0, sizeof(buffer_struct) );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::update( pcvoid in_data, u32 in_offset, u32 in_size ) const
-{
-	buffer::update_dynamic( in_data, in_offset, in_size );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::bind_vs( u32 in_slot ) const
-{
-	api::get_context( )->VSSetConstantBuffers( in_slot, 1, &m_buffer );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::bind_ps( u32 in_slot ) const
-{
-	api::get_context( )->PSSetConstantBuffers( in_slot, 1, &m_buffer );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::bind_gs( u32 in_slot ) const
-{
-	api::get_context( )->GSSetConstantBuffers( in_slot, 1, &m_buffer );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::bind_hs( u32 in_slot ) const
-{
-	api::get_context( )->HSSetConstantBuffers( in_slot, 1, &m_buffer );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::bind_ds( u32 in_slot ) const
-{
-	api::get_context( )->DSSetConstantBuffers( in_slot, 1, &m_buffer );
-}
-
-template<typename buffer_struct>
-void constant_buffer<buffer_struct>::bind_cs( u32 in_slot ) const
-{
-	api::get_context( )->CSSetConstantBuffers( in_slot, 1, &m_buffer );
+	update( &in_data, 0, sizeof(buffer_struct) );
 }
 
 } // namespace render
