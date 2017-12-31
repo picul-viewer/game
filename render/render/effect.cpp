462d70cd564987d291258b8f76922470e76f951e
repeater <none@none>
2017-12-31 03:38:03 +0200

    implement effect class properly
    --HG--
    branch : render v0

--- render/render/effect.cpp
+++ render/render/effect.cpp
@@ -4,20 +4,22 @@
 
 namespace render {
 
-void effect::apply( u8					in_stencil_ref,
-					math::float4 const&	in_blend_factor,
-					u32					in_sample_mask ) const
+void technique::create( )
 {
-	m_depth_stencil_state->bind( in_stencil_ref );
-	m_blend_state->bind( in_blend_factor, in_sample_mask );
-	m_rasterizer_state->bind( );
-
-	m_vertex_shader->bind( );
-	m_geometry_shader->bind( );
-	m_pixel_shader->bind( );
+	m_depth_stencil_state	= nullptr;
+	m_blend_state			= nullptr;
+	m_rasterizer_state		= nullptr;
+	
+	m_vertex_shader			= nullptr;
+	m_pixel_shader			= nullptr;
+	m_geometry_shader		= nullptr;
+	
+	m_blend_factor			= math::float4( 1.0f, 1.0f, 1.0f, 1.0f );
+	m_sample_mask			= 0xFFFFFFFF;
+	m_stencil_ref			= 0;
 }
 
-void effect::destroy( )
+void technique::destroy( )
 {
 	if ( !m_vertex_shader->is_null( ) )
 		resources::get_shader_pool( ).free_vertex	( m_vertex_shader );
@@ -27,34 +29,66 @@ void effect::destroy( )
 		resources::get_shader_pool( ).free_geometry	( m_geometry_shader );
 }
 
-void effect::set_vertex_shader( pcstr in_filename, u32 in_configuration )
+void technique::apply( ) const
+{
+	if ( m_depth_stencil_state )
+		m_depth_stencil_state->bind( m_stencil_ref );
+	if ( m_blend_state )
+		m_blend_state->bind( m_blend_factor, m_sample_mask );
+	if ( m_rasterizer_state )
+		m_rasterizer_state->bind( );
+
+	if ( m_vertex_shader )
+		m_vertex_shader->bind( );
+	if ( m_geometry_shader )
+		m_geometry_shader->bind( );
+	if ( m_pixel_shader )
+		m_pixel_shader->bind( );
+}
+
+void technique::set_vertex_shader( pcstr in_filename, u32 in_configuration )
 {
 	m_vertex_shader = resources::get_shader_pool( ).create_vertex( in_filename, in_configuration );
 }
 
-void effect::set_pixel_shader( pcstr in_filename, u32 in_configuration )
+void technique::set_pixel_shader( pcstr in_filename, u32 in_configuration )
 {
 	m_pixel_shader = resources::get_shader_pool( ).create_pixel( in_filename, in_configuration );
 }
 
-void effect::set_geometry_shader( pcstr in_filename, u32 in_configuration )
+void technique::set_geometry_shader( pcstr in_filename, u32 in_configuration )
 {
 	m_geometry_shader = resources::get_shader_pool( ).create_geometry( in_filename, in_configuration );
 }
 
-void effect::set_depth_stencil_state( depth_stencil_state::cook const& in_cook )
+void technique::set_depth_stencil_state( depth_stencil_state::cook const& in_cook )
 {
 	m_depth_stencil_state = resources::get_pipeline_pool( ).create_depth_stencil( in_cook );
 }
 
-void effect::set_blend_state( blend_state::cook const& in_cook )
+void technique::set_blend_state( blend_state::cook const& in_cook )
 {
 	m_blend_state = resources::get_pipeline_pool( ).create_blend( in_cook );
 }
 
-void effect::set_rasterizer_state( rasterizer_state::cook const& in_cook )
+void technique::set_rasterizer_state( rasterizer_state::cook const& in_cook )
 {
 	m_rasterizer_state = resources::get_pipeline_pool( ).create_rasterizer( in_cook );
+}
+
+void technique::set_stencil_ref( u8 in_stencil_ref )
+{
+	m_stencil_ref = in_stencil_ref;
+}
+
+void technique::set_blend_factor( math::float4 in_blend_factor )
+{
+	m_blend_factor = in_blend_factor;
+}
+
+void technique::set_sample_mask( u32 in_sample_mask )
+{
+	m_sample_mask = in_sample_mask;
 }
 
 } // namespace render
---
+++ render/render/effect_inline.h
@@ -0,0 +1,29 @@
+#ifndef __render_effect_inline_h_included_
+#define __render_effect_inline_h_included_
+
+namespace render {
+
+template<uptr TechniqueCount>
+technique& effect<TechniqueCount>::operator[]( uptr in_index )
+{
+	ASSERT( in_index < TechniqueCount );
+	return m_techniques[in_index];
+}
+
+template<uptr TechniqueCount>
+technique const& effect<TechniqueCount>::operator[]( uptr in_index ) const
+{
+	ASSERT( in_index < TechniqueCount );
+	return m_techniques[in_index];
+}
+
+template<uptr TechniqueCount>
+void effect<TechniqueCount>::apply( uptr in_index ) const
+{
+	ASSERT( in_index < TechniqueCount );
+	m_techniques[in_index].apply( );
+}
+
+} // namespace render
+
+#endif // #ifndef __render_effect_inline_h_included_
