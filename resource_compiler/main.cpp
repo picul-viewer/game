c234d5523dc83c39a17a391abb8b9b8ae0e198f1
repeater <none@none>
2018-01-14 01:10:39 +0200

    rewrite main
    --HG--
    branch : resource compiler

--- resource_compiler/main.cpp
+++ resource_compiler/main.cpp
@@ -2,13 +2,28 @@
 
 #include "resource_compiler.h"
 
+#include "shader_strategy.h"
+
 using namespace resource_compiler;
 
 int main( int argc, char** argv )
 {
 	ASSERT( argc >= 3 );
 
-	manager manager( { } );
+	manager manager( 1 );
+
+	{
+		bool debug = 
+#ifdef DEBUG
+			true
+#else
+			false
+#endif // #ifdef DEBUG
+			;
+		
+		manager.add_category( new shader_strategy( "4_0", debug ), "shaders/compile.cfg", "shaders/4_0/" );
+		manager.add_category( new shader_strategy( "5_0", debug ), "shaders/compile.cfg", "shaders/5_0/" );
+	}
 
 	manager.compile( argv[1], argv[2] );
 }
