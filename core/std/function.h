4836f705b358fcc2bf028664a14fb474d778f5cf
repeater <none@none>
2018-02-22 03:00:57 +0200

    add function_binding struct

--- core/std/function.h
+++ core/std/function.h
@@ -4,6 +4,396 @@
 #include <core/types.h>
 #include <core/macros.h>
 
+template<u32 index>
+struct bind_parameter_type
+{
+	enum { value = index };
+};
+
+static const bind_parameter_type<0> bind_param_0;
+static const bind_parameter_type<1> bind_param_1;
+static const bind_parameter_type<2> bind_param_2;
+static const bind_parameter_type<3> bind_param_3;
+static const bind_parameter_type<4> bind_param_4;
+static const bind_parameter_type<5> bind_param_5;
+static const bind_parameter_type<6> bind_param_6;
+static const bind_parameter_type<7> bind_param_7;
+static const bind_parameter_type<8> bind_param_8;
+static const bind_parameter_type<9> bind_param_9;
+
+template<typename FunctionType>
+struct function_binding;
+
+template<typename FunctionReturn, typename ... FunctionArgs>
+struct function_binding<FunctionReturn(*)( FunctionArgs ... )>
+{
+public:
+	template<typename FunctorType, typename ... Args>
+	inline function_binding( FunctorType const& func, Args ... args )
+	{
+		typedef invoke_function_helper<FunctorType> helper;
+
+		invoker			= &helper::invoke<Args ...>;
+		helper::create	( data, func, args ... );
+	}
+
+	FunctionReturn operator( )( FunctionArgs ... args )
+	{
+		return (FunctionReturn)invoker( data, args ... );
+	}
+
+protected:
+	typedef FunctionReturn(*invoker_type)( pbyte, FunctionArgs ... );
+
+	
+	template<typename ArgsType, u32 Index>
+	struct type_by_index;
+
+	
+	template<typename ... ArgPair>
+	struct arg_pair_type;
+	
+	template<>
+	struct arg_pair_type<>
+	{
+		typedef void left_type;
+		typedef void right_type;
+	};
+	
+	template<typename T>
+	struct arg_pair_type<T>
+	{
+		typedef T left_type;
+		typedef arg_pair_type<> right_type;
+		
+		left_type left;
+
+		template<u32 Index>
+		inline typename type_by_index<arg_pair_type<T>, Index>::value_type value_by_index( )
+		{
+			static_assert( false, "Parameter index out of bounds" );
+		}
+		
+		template<>
+		inline typename type_by_index<arg_pair_type<T>, 0>::value_type value_by_index<0>( )
+		{
+			return left;
+		}
+	};
+
+	template<typename L, typename R>
+	struct arg_pair_type<L, R>
+	{
+		typedef L left_type;
+		typedef R right_type;
+
+		left_type left;
+		right_type right;
+		
+		template<u32 Index>
+		inline typename type_by_index<arg_pair_type<L, R>, Index>::value_type value_by_index( )
+		{
+			return right.value_by_index<Index - 1>( );
+		}
+		
+		template<>
+		inline typename type_by_index<arg_pair_type<L, R>, 0>::value_type value_by_index<0>( )
+		{
+			return left;
+		}
+	};
+
+	
+	template<typename ArgsType, u32 Index>
+	struct type_by_index
+	{
+		typedef typename type_by_index<typename ArgsType::right_type, Index - 1>::value_type value_type;
+	};
+	
+	template<typename ArgsType>
+	struct type_by_index<ArgsType, 0u>
+	{
+		typedef typename ArgsType::left_type value_type;
+	};
+	
+	template<typename T, u32 Index>
+	struct type_by_index<arg_pair_type<T>, Index>
+	{ };
+	
+	template<typename T>
+	struct type_by_index<arg_pair_type<T>, 0>
+	{
+		typedef T value_type;
+	};
+
+	template<u32 Index>
+	struct type_by_index<arg_pair_type<>, Index>
+	{ };
+
+
+	template<typename ... Args>
+	struct args_store_type;
+	
+	template<>
+	struct args_store_type<>
+	{
+		typedef arg_pair_type<> pair_type;
+
+		args_store_type( ) = default;
+		
+		operator pair_type( )
+		{
+			return *(pair_type*)this;
+		}
+	};
+	
+	template<typename T>
+	struct args_store_type<T>
+	{
+		typedef arg_pair_type<T> pair_type;
+
+		args_store_type( T left ) :
+			left	( left )
+		{ }
+		
+		operator pair_type( )
+		{
+			return *(pair_type*)this;
+		}
+
+		T left;
+	};
+
+	template<typename T, typename ... Args>
+	struct args_store_type<T, Args ...>
+	{
+		typedef arg_pair_type<T, typename args_store_type<Args ...>::pair_type> pair_type;
+
+		args_store_type( T left, Args ... right ) :
+			left	( left ),
+			right	( right ... )
+		{ }
+
+		operator pair_type( )
+		{
+			return *(pair_type*)this;
+		}
+
+		T left;
+		args_store_type<Args ...> right;
+	};
+
+	
+	template<typename FunctorType, typename CallerArgsType, typename ArgsType, typename ... Args>
+	struct invoke_helper;
+	
+	template<typename FunctorType, typename CallerArgsType, typename LeftType, typename RightArgsType, typename ... Args>
+	struct invoke_helper<FunctorType, CallerArgsType, arg_pair_type<LeftType, RightArgsType>, Args ...>
+	{
+		static inline typename FunctorType::return_type call( FunctorType const& functor, CallerArgsType caller_args, arg_pair_type<LeftType, RightArgsType>& args_holder, Args ... args )
+		{
+			return invoke_helper<FunctorType, CallerArgsType, RightArgsType, Args ..., LeftType>::call(
+				functor, caller_args, args_holder.right, args ..., args_holder.left );
+		}
+	};
+	
+	template<typename FunctorType, typename CallerArgsType, u32 BindIndex, typename RightArgsType, typename ... Args>
+	struct invoke_helper<FunctorType, CallerArgsType, arg_pair_type<bind_parameter_type<BindIndex>, RightArgsType>, Args ...>
+	{
+		static inline typename FunctorType::return_type call( FunctorType const& functor, CallerArgsType caller_args, arg_pair_type<bind_parameter_type<BindIndex>, RightArgsType>& args_holder, Args ... args )
+		{
+			return invoke_helper<FunctorType, CallerArgsType, RightArgsType, Args ..., typename type_by_index<CallerArgsType, BindIndex>::value_type>::call(
+				functor, caller_args, args_holder.right, args ..., caller_args.value_by_index<BindIndex>( ) );
+		}
+	};
+	
+	template<typename FunctorType, typename CallerArgsType, typename T, typename ... Args>
+	struct invoke_helper<FunctorType, CallerArgsType, arg_pair_type<T>, Args ...>
+	{
+		static inline typename FunctorType::return_type call( FunctorType const& functor, CallerArgsType caller_args, arg_pair_type<T>& args_holder, Args ... args )
+		{
+			return invoke_helper<FunctorType, CallerArgsType, arg_pair_type<>, Args ..., T>::call(
+				functor, caller_args, arg_pair_type<>( ), args ..., args_holder.left );
+		}
+	};
+	
+	template<typename FunctorType, typename CallerArgsType, u32 BindIndex, typename ... Args>
+	struct invoke_helper<FunctorType, CallerArgsType, arg_pair_type<bind_parameter_type<BindIndex>>, Args ...>
+	{
+		static inline typename FunctorType::return_type call( FunctorType const& functor, CallerArgsType caller_args, arg_pair_type<bind_parameter_type<BindIndex>>& args_holder, Args ... args )
+		{
+			return invoke_helper<FunctorType, CallerArgsType, arg_pair_type<>, Args ..., type_by_index<CallerArgsType, BindIndex>::value_type>::call(
+				functor, caller_args, arg_pair_type<>( ), args ..., caller_args.value_by_index<BindIndex>( ) );
+		}
+	};
+
+	template<typename FunctorType, typename CallerArgsType, typename ... Args>
+	struct invoke_helper<FunctorType, CallerArgsType, arg_pair_type<>, Args ...>
+	{
+		static inline typename FunctorType::return_type call( FunctorType const& functor, CallerArgsType function_args, arg_pair_type<>& args_holder, Args ... args )
+		{
+			return functor( args ... );
+		}
+	};
+
+	
+	template<typename FunctorType>
+	struct invoke_function_helper;
+
+	typedef typename args_store_type<FunctionArgs ...>::pair_type func_params_type;
+
+	template<typename Return, typename ... FArgs>
+	struct invoke_function_helper<Return(*)( FArgs ... )>
+	{
+		typedef Return(*function_type)( FArgs ... );
+
+		struct bind
+		{
+		public:
+			typedef Return(*value_type)( FArgs ... );
+			typedef Return return_type;
+
+			inline bind( value_type method ) :
+				m_function	( method )
+			{ }
+
+			inline Return operator( )( FArgs ... args ) const
+			{
+				return m_function( args ... );
+			}
+
+		protected:
+			value_type	m_function;
+		};
+		
+		template<typename ... Args>
+		static inline void create( pbyte arena, function_type func, Args ... args )
+		{
+			ASSERT( sizeof(function_type) + sizeof(args_store_type<Args ...>) <= ArenaSize, "not enough memory to store all arguments" );
+
+			*(function_type*)( arena ) = func;
+			new ( arena + sizeof(function_type) ) args_store_type<Args ...>( args ... );
+		}
+
+		template<typename ... Args>
+		static inline Return invoke( pbyte arena, FunctionArgs ... args )
+		{
+			typedef typename args_store_type<Args ...>::pair_type params_type;
+
+			function_type const&	func_value = *(function_type*)( arena );
+			params_type&			args_value = *(params_type*)( arena + sizeof(function_type) );
+
+			return invoke_helper<bind, func_params_type, params_type>::call( bind( func_value ), args_store_type<FunctionArgs ...>( args ... ), args_value );
+		}
+	};
+	
+	template<typename Return, typename This, typename ... FArgs>
+	struct invoke_function_helper<Return(This::*)( FArgs ... )>
+	{
+		typedef Return(This::*function_type)( FArgs ... );
+
+		struct bind
+		{
+		public:
+			typedef Return(This::*value_type)( FArgs ... );
+			typedef Return return_type;
+
+			inline bind( value_type method, This* this_ptr ) :
+				m_method	( method ),
+				m_this_ptr	( this_ptr )
+			{ }
+
+			inline Return operator( )( FArgs ... args ) const
+			{
+				return ( m_this_ptr->*m_method )( args ... );
+			}
+
+		protected:
+			value_type	m_method;
+			This*		m_this_ptr;
+		};
+		
+		template<typename ... Args>
+		static inline void create( pbyte arena, function_type func, This* this_ptr, Args ... args )
+		{
+			ASSERT( sizeof(function_type) + sizeof(This*) + sizeof(args_store_type<Args ...>) <= ArenaSize, "not enough memory to store all arguments" );
+
+			*(function_type*)( arena ) = func;
+			*(This**)( arena + sizeof(function_type) ) = this_ptr;
+			new ( arena + sizeof(function_type) + sizeof(This*) ) args_store_type<Args ...>( args ... );
+		}
+		
+		template<typename ... Args>
+		static inline Return invoke( pbyte arena, FunctionArgs ... args )
+		{
+			typedef typename args_store_type<Args ...>::pair_type params_type;
+
+			function_type	func_value = *(function_type*)( arena );
+			This*			this_value = *(This**)( arena + sizeof(function_type) );
+			params_type&	args_value = *(params_type*)( arena + sizeof(function_type) + sizeof(This*) );
+
+			return invoke_helper<bind, func_params_type, params_type>::call( bind( func_value, this_value ), args_store_type<FunctionArgs ...>( args ... ), args_value );
+		}
+	};
+	
+	template<typename Return, typename This, typename ... FArgs>
+	struct invoke_function_helper<Return(This::*)( FArgs ... ) const>
+	{
+		typedef Return(This::*function_type)( FArgs ... ) const;
+
+		struct bind
+		{
+		public:
+			typedef Return(This::*value_type)( FArgs ... ) const;
+			typedef Return return_type;
+
+			inline bind( value_type method, This const* this_ptr ) :
+				m_method	( method ),
+				m_this_ptr	( this_ptr )
+			{ }
+
+			inline Return operator( )( FArgs ... args ) const
+			{
+				return ( m_this_ptr->*m_method )( args ... );
+			}
+
+		protected:
+			value_type	m_method;
+			This const*	m_this_ptr;
+		};
+		
+		template<typename ... Args>
+		static inline void create( pbyte arena, function_type func, This* this_ptr, Args ... args )
+		{
+			ASSERT( sizeof(function_type) + sizeof(This const*) + sizeof(args_store_type<Args ...>) <= ArenaSize, "not enough memory to store all arguments" );
+
+			*(function_type*)( arena ) = func;
+			*(This const**)( arena + sizeof(function_type) ) = this_ptr;
+			new ( arena + sizeof(function_type) + sizeof(This const*) ) args_store_type<Args ...>( args ... );
+		}
+		
+		template<typename ... Args>
+		static inline Return invoke( pbyte arena, Args ... args )
+		{
+			typedef typename args_store_type<Args ...>::pair_type params_type;
+
+			function_type	func_value = *(function_type*)( arena );
+			This const*		this_value = *(This const**)( arena + sizeof(function_type) );
+			params_type&	args_value = *(params_type*)( arena + sizeof(function_type) + sizeof(This const*) );
+
+			return invoke_helper<bind, func_params_type, params_type>::call( bind( func_value, this_value ), args_store_type<FunctionArgs ...>( args ... ), args_value );
+		}
+	};
+	
+protected:
+	enum { Size = 128 };
+	enum { ArenaSize = Size - sizeof(invoker_type) };
+
+	invoker_type invoker;
+	byte data[ArenaSize];
+
+};
+
 template<typename T, typename Return, typename ... Args>
 struct method_binding
 {
