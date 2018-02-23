6917ff786b1d383c866bc18500947b4240ca9993
repeater <none@none>
2018-02-24 01:14:53 +0200

    rename "std" core submodule to "lib" and remove all-inclusive files

--- core/system.h
+++
@@ -1,8 +0,0 @@
-#ifndef __core_system_h_included_
-#define __core_system_h_included_
-
-#include "system/timer.h"
-
-#include "system/window.h"
-
-#endif // #ifndef __core_system_h_included_
--- core/threads.h
+++
@@ -1,9 +0,0 @@
-#ifndef __core_threads_h_included_
-#define __core_threads_h_included_
-
-#include "threads/thread.h"
-#include "threads/threading_event.h"
-#include "threads/interlocked.h"
-#include "threads/spsc_queue.h"
-
-#endif // #ifndef __core_threads_h_included_
