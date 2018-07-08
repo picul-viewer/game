3a0a0202ac2874247e151ce71265e4dc75445faf
repeater <none@none>
2018-07-08 20:08:31 +0300

    system: rework window class

---
+++ core/system/window.cpp
@@ -0,0 +1,81 @@
+#include "window.h"
+
+#include <Windows.h>
+#include <windowsx.h>
+#include <macros.h>
+
+namespace sys {
+
+void window::create( pcstr title, math::s32x2 const& dimensions, bool fullscreen, pointer window_procedure )
+{
+	ASSERT				( dimensions > 0 );
+
+	HINSTANCE inst		= GetModuleHandle( nullptr );
+
+	WNDCLASSEX wcex;
+	wcex.cbSize			= sizeof(WNDCLASSEX);
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= window_procedure;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= inst;
+	wcex.hbrBackground	= (HBRUSH)( COLOR_WINDOW + 1 );
+	wcex.lpszMenuName	= nullptr;
+	wcex.lpszClassName	= title;
+	wcex.hIcon			= LoadIcon	( inst, IDI_APPLICATION );
+	wcex.hCursor		= LoadCursor( nullptr, IDC_ARROW );
+	wcex.hIconSm		= LoadIcon	( wcex.hInstance, IDI_APPLICATION );
+
+	auto result			= RegisterClassEx( &wcex );
+	ASSERT				( result != 0 );
+
+	RECT rect			= { 0, 0, dimensions.x, dimensions.y };
+
+	DWORD style			= ( fullscreen ? ( WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN ) : WS_OVERLAPPEDWINDOW ) | WS_VISIBLE;
+
+	AdjustWindowRect	( &rect, style, FALSE );
+	
+	s32 width			= rect.right - rect.left;
+	s32 height			= rect.bottom - rect.top;
+
+	s32 x				= ( GetSystemMetrics( SM_CXSCREEN ) - width ) / 2;
+	s32 y				= ( GetSystemMetrics( SM_CYSCREEN ) - height ) / 2;
+
+	m_hwnd				= CreateWindow( title, title, style, x, y, width, height, nullptr, nullptr, inst, this );
+
+	ASSERT				( m_hwnd );
+
+	ShowWindow			( (HWND)m_hwnd, SW_SHOW );
+}
+
+void window::destroy( )
+{
+	ASSERT				( m_hwnd );
+
+	enum { class_name_max_length = 256 };
+	char class_name[class_name_max_length];
+	RealGetWindowClass	( (HWND)m_hwnd, class_name, class_name_max_length );
+
+	DestroyWindow		( (HWND)m_hwnd );
+	m_hwnd				= nullptr;
+	
+	HINSTANCE inst		= GetModuleHandle( nullptr );
+	UnregisterClass		( class_name, inst );
+}
+
+void window::run( )
+{
+	MSG msg;
+
+	do
+	{
+		if ( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE ) )
+		{
+			TranslateMessage( &msg );
+			DispatchMessage	( &msg );
+		}
+	}
+	while ( msg.message != WM_QUIT );
+}
+
+} // namespace sys
--- core/system/window.h
+++ core/system/window.h
@@ -3,35 +3,25 @@
 
 #include <types.h>
 #include <math/vector.h>
-#include <Windows.h>
 
-struct window_init
-{
-	pcstr title;
-	math::s16x2 dimensions;
-	bool fullscreen;
-	bool visible;
-};
+namespace sys {
 
-template<typename Events>
 class window
 {
 public:
 	window( ) = default;
 	~window( ) = default;
 
-	void create( window_init const& init );
+	void create( pcstr title, math::s32x2 const& dimensions, bool fullscreen, pointer window_procedure );
 	void destroy( );
-	void show( int param );
-	bool is_opened( );
 	void run( );
 
-	inline HWND get_hwnd( ) const { return m_hwnd; }
+	inline pvoid get_hwnd( ) const { return m_hwnd; }
 
 protected:
-	HWND		m_hwnd;
+	pvoid m_hwnd;
 
-	static LRESULT CALLBACK wndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam );
+	//static LRESULT CALLBACK wndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam );
 };
 
 enum mouse_button
@@ -43,7 +33,7 @@ enum mouse_button
 
 struct window_default_events
 {
-	static void create( HWND hwnd ) { }
+	//static void create( HWND hwnd ) { }
 	static void destroy( ) { }
 	static void activate( bool active ) { }
 	static void resize( math::u16x2 new_dimensions ) { }
@@ -57,6 +47,6 @@ struct window_default_events
 	static void key_up( u64 virtual_key ) { }
 };
 
-#include "window_inline.h"
+} // namespace sys
 
 #endif // #ifndef __core_window_h_included_
