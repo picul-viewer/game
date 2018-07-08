0c9ce45b3f4908c90f08720f5fd191cd55c698c9
repeater <none@none>
2018-07-09 01:18:48 +0300

    system: remove redudant code

--- core/system/window.h
+++ core/system/window.h
@@ -20,31 +20,6 @@ public:
 
 protected:
 	pvoid m_hwnd;
-
-	//static LRESULT CALLBACK wndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam );
-};
-
-enum mouse_button
-{
-	left = 0,
-	middle,
-	right,
-};
-
-struct window_default_events
-{
-	//static void create( HWND hwnd ) { }
-	static void destroy( ) { }
-	static void activate( bool active ) { }
-	static void resize( math::u16x2 new_dimensions ) { }
-	static void update( ) { }
-
-	static void mouse_move( math::s16x2 cursor_position ) { }
-	static void mouse_button_down( u32 button_id ) { }
-	static void mouse_button_up( u32 button_id ) { }
-	static void mouse_wheel( s16 angle ) { }
-	static void key_down( u64 virtual_key ) { }
-	static void key_up( u64 virtual_key ) { }
 };
 
 } // namespace sys
