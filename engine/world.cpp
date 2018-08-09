06f797ececa735b1c8a79ea7df126da6b3e19fa5
repeater <none@none>
2018-08-09 22:07:00 +0300

    engine: add game logic support

--- engine/world.cpp
+++ engine/world.cpp
@@ -44,17 +44,9 @@ static LRESULT CALLBACK window_procedure( HWND hwnd, UINT message, WPARAM w_para
 			g_world.window_input( (pvoid)l_param );
 			break;
 		}
-
-		default:
-			return DefWindowProcA( hwnd, message, w_param, l_param );
 	}
 
-	return 0;
-}
-
-static void window_thread( pvoid )
-{
-	g_world.window_thread( );
+	return DefWindowProc( hwnd, message, w_param, l_param );
 }
 
 };
@@ -66,7 +58,7 @@ void world::window_resize( math::u32x2 const& new_dimensions )
 
 	m_window_dimensions = new_dimensions;
 
-	// Resize
+	game::world_interface::window_resize( new_dimensions );
 }
 
 void world::window_activate( bool is_active )
@@ -76,33 +68,67 @@ void world::window_activate( bool is_active )
 
 	m_is_active = is_active;
 
-	// Activate/deactivate
+	game::world_interface::window_activate( is_active );
 }
 
 void world::window_input( pvoid handle )
 {
-	// Input
+	game::world_interface::window_input( handle );
+}
+
+void world::render_thread( )
+{
+	while (!m_window_created)
+		_mm_pause( );
+
+	ASSERT( ( ( m_window_dimensions.x & 0xFFFF ) == m_window_dimensions.x ) &&
+			( ( m_window_dimensions.y & 0xFFFF ) == m_window_dimensions.y ) );
+
+	render::g_world.run( (HWND)m_window.get_hwnd( ), m_window_dimensions, true, true );
+	
+	LOG( "engine: render exit\n" );
+
+	m_window.exit( );
 }
 
 void world::window_thread( )
 {
+	sys::g_window_input.create( );
+	
 	m_window.create( "game", m_window_dimensions, false, &helper::window_procedure );
 	m_window_created = true;
+
 	m_window.run( );
 	m_window.destroy( );
+	
+	sys::g_window_input.destroy( );
+
+	LOG( "engine: window exit\n" );
+	
+	game::world_interface::exit( );
+}
+
+void world::game_thread( )
+{
+	game::world_interface::run( );
+	
+	LOG( "engine: game logic exit\n" );
+
+	render::g_world.exit( );
 }
 
 void world::initialize( )
 {
 	m_window_created = false;
-	m_alive = true;
 
-	m_window_thread.create( &helper::window_thread, 1 * Mb, nullptr );
+	m_threads[thread_window].create( thread::method_helper<world, &world::window_thread>, 1 * Mb, this );
+
+	m_threads[thread_game].create( thread::method_helper<world, &world::game_thread>, 4 * Mb, this );
 }
 
 void world::deinitialize( )
 {
-	m_window_thread.destroy( INFINITE );
+	m_threads->destroy( INFINITE, thread_count );
 }
 
 void world::run( math::u32x2 in_window_dimensions )
@@ -111,20 +137,21 @@ void world::run( math::u32x2 in_window_dimensions )
 
 	initialize( );
 
-	while ( !m_window_created )
-		_mm_pause( );
-
-	ASSERT( ( ( m_window_dimensions.x & 0xFFFF ) == m_window_dimensions.x ) &&
-			( ( m_window_dimensions.y & 0xFFFF ) == m_window_dimensions.y ) );
-
-	render::g_world.run( (HWND)m_window.get_hwnd( ), m_window_dimensions, true, true );
+	// Do render in the main thread.
+	render_thread( );
 
 	deinitialize( );
 }
 
 void world::exit( )
 {
-	m_alive = false;
+	/*
+		Current destruction sequence:
+		1. Window
+		2. Game logic thread
+		3. Render thread
+	*/
+	m_window.exit( );
 }
 
 scene* world::create_scene( ) const
--- engine/world.h
+++ engine/world.h
@@ -33,27 +33,56 @@ protected:
 	void window_resize( math::u32x2 const& new_dimensions );
 	void window_activate( bool is_active );
 	void window_input( pvoid handle );
-
+	
+	void render_thread( );
 	void window_thread( );
+	void game_thread( );
 
 	void initialize( );
 	void deinitialize( );
 
 protected:
+	enum threads
+	{
+		thread_window = 0,
+		thread_game,
+
+		thread_count
+	};
+
+protected:
 	sys::window m_window;
 
-	thread m_window_thread;
+	thread m_threads[thread_count];
 
 	math::u32x2 m_window_dimensions;
 	bool m_is_active;
 
 	volatile bool m_window_created;
-	volatile bool m_alive;
 
 };
 
 extern world g_world;
 
 } // namespace engine
+
+/*	World interface is the interface for communication with game-logic project.
+	This defined as namespace because it's stateless. Actual state supposed to
+	be encapsulated in "game::world" class, and this interface's functions are
+	supposed to be implemented as calls to corresponding methods of this class. */
+namespace game {
+
+namespace world_interface {
+
+void run( );
+void exit( );
+
+void window_resize( math::u32x2 const& new_dimensions );
+void window_activate( bool is_active );
+void window_input( pvoid handle );
+
+} // namespace world_interface
+
+} // namespace game
 
 #endif // #ifndef __engine_world_h_included_
