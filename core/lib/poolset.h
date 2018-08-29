92f703d97fe9214f0a5475ddd3821fbae0f74cf5
repeater <none@none>
2018-08-29 14:24:32 +0300

    lib: fix error in poolset classes

--- core/lib/poolset.h
+++ core/lib/poolset.h
@@ -9,12 +9,12 @@ template<template<uptr, uptr> class Base, uptr PageSize, typename ... TList>
 struct base_poolset;
 
 template<template<uptr, uptr> class Base, uptr PageSize, typename T>
-struct base_poolset<Base, PageSize, T> : Base<sizeof(T), PageSize / sizeof(T)>
+struct base_poolset<Base, PageSize, T> : Base<sizeof(T), PageSize>
 {
 public:
-	using Base<sizeof(T), PageSize / sizeof(T)>::create;
-	using Base<sizeof(T), PageSize / sizeof(T)>::destroy;
-	using Base<sizeof(T), PageSize / sizeof(T)>::clear;
+	using Base<sizeof(T), PageSize>::create;
+	using Base<sizeof(T), PageSize>::destroy;
+	using Base<sizeof(T), PageSize>::clear;
 
 	template<typename U>
 	U* allocate( )
@@ -31,13 +31,13 @@ public:
 	template<>
 	T* allocate<T>( )
 	{
-		return Base<sizeof(T), PageSize / sizeof(T)>::allocate( sizeof(T) );
+		return Base<sizeof(T), PageSize>::allocate( sizeof(T) );
 	}
 
 	template<>
 	void deallocate<T>( T* p )
 	{
-		Base<sizeof(T), PageSize / sizeof(T)>::deallocate( p );
+		Base<sizeof(T), PageSize>::deallocate( p );
 	}
 
 	u32 get_object_type( pointer object )
@@ -46,8 +46,8 @@ public:
 		auto f = [&]( )
 		{
 			return
-				( object >= Base<sizeof(T), PageSize / sizeof(T)>::m_data ) &&
-				( object <  Base<sizeof(T), PageSize / sizeof(T)>::m_data + pool_page_size_helper<PageSize>::value );
+				( object >= Base<sizeof(T), PageSize>::m_data ) &&
+				( object <  Base<sizeof(T), PageSize>::m_data + pool_page_size_helper<PageSize>::value );
 		};
 
 		ASSERT( f( ) );
@@ -58,25 +58,25 @@ public:
 
 template<template<uptr, uptr> class Base, uptr PageSize, typename T, typename ... TList>
 struct base_poolset<Base, PageSize, T, TList ...> :
-	Base<sizeof(T), PageSize / sizeof(T)>,
+	Base<sizeof(T), PageSize>,
 	base_poolset<Base, PageSize, TList ...>
 {
 public:
 	void create( )
 	{
-		Base<sizeof(T), PageSize / sizeof(T)>::create( virtual_mem_allocator( ).commit( nullptr, pool_page_size_helper<PageSize>::value * type_count<T, TList ...>::value ) );
-		base_poolset<Base, PageSize, TList ...>::create( Base<sizeof(T), PageSize / sizeof(T)>::m_data + pool_page_size_helper<PageSize>::value );
+		Base<sizeof(T), PageSize>::create( virtual_mem_allocator( ).commit( nullptr, pool_page_size_helper<PageSize>::value * type_count<T, TList ...>::value ) );
+		base_poolset<Base, PageSize, TList ...>::create( Base<sizeof(T), PageSize>::m_data + pool_page_size_helper<PageSize>::value );
 	}
 
 	void destroy( )
 	{
-		Base<sizeof(T), PageSize / sizeof(T)>::destroy( );
+		Base<sizeof(T), PageSize>::destroy( );
 		base_poolset<Base, PageSize, TList ...>::destroy( );
 	}
 	
 	void clear( )
 	{
-		Base<sizeof(T), PageSize / sizeof(T)>::clear( );
+		Base<sizeof(T), PageSize>::clear( );
 		base_poolset<Base, PageSize, TList ...>::clear( );
 	}
 	
@@ -95,13 +95,13 @@ public:
 	template<>
 	T* allocate<T>( )
 	{
-		return Base<sizeof(T), PageSize / sizeof(T)>::allocate( sizeof(T) );
+		return Base<sizeof(T), PageSize>::allocate( sizeof(T) );
 	}
 
 	template<>
 	void deallocate<T>( T* p )
 	{
-		Base<sizeof(T), PageSize / sizeof(T)>::deallocate( p );
+		Base<sizeof(T), PageSize>::deallocate( p );
 	}
 	
 	u32 get_object_type( pointer object )
@@ -110,19 +110,19 @@ public:
 		auto f = [&]( )
 		{
 			return
-				( object >= Base<sizeof(T), PageSize / sizeof(T)>::m_data ) &&
-				( object <  Base<sizeof(T), PageSize / sizeof(T)>::m_data + pool_page_size_helper<PageSize>::value * type_count<T, TList ...>::value );
+				( object >= Base<sizeof(T), PageSize>::m_data ) &&
+				( object <  Base<sizeof(T), PageSize>::m_data + pool_page_size_helper<PageSize>::value * type_count<T, TList ...>::value );
 		};
 
 		ASSERT( f( ) );
 #endif // #ifdef DEBUG
-		return (u32)( ( object - Base<sizeof(T), PageSize / sizeof(T)>::m_data ) / pool_page_size_helper<PageSize>::value );
+		return (u32)( ( object - Base<sizeof(T), PageSize>::m_data ) / pool_page_size_helper<PageSize>::value );
 	}
 
 protected:
 	void create( pointer memory )
 	{
-		Base<sizeof(T), PageSize / sizeof(T)>::create( memory );
+		Base<sizeof(T), PageSize>::create( memory );
 		base_poolset<Base, PageSize, TList ...>::create( memory + pool_page_size_helper<PageSize>::value );
 	}
 };
@@ -132,12 +132,12 @@ template<template<uptr, uptr, uptr> class Base, uptr PageSize, uptr PageMaxCount
 struct base_dynamic_poolset;
 
 template<template<uptr, uptr, uptr> class Base, uptr PageSize, uptr PageMaxCount, typename T>
-struct base_dynamic_poolset<Base, PageSize, PageMaxCount, T> : Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>
+struct base_dynamic_poolset<Base, PageSize, PageMaxCount, T> : Base<sizeof(T), PageSize, PageMaxCount>
 {
 public:
-	using Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::create;
-	using Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::destroy;
-	using Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::clear;
+	using Base<sizeof(T), PageSize, PageMaxCount>::create;
+	using Base<sizeof(T), PageSize, PageMaxCount>::destroy;
+	using Base<sizeof(T), PageSize, PageMaxCount>::clear;
 
 	template<typename U>
 	U* allocate( )
@@ -154,13 +154,13 @@ public:
 	template<>
 	T* allocate<T>( )
 	{
-		return Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::allocate( sizeof(T) );
+		return Base<sizeof(T), PageSize, PageMaxCount>::allocate( sizeof(T) );
 	}
 
 	template<>
 	void deallocate<T>( T* p )
 	{
-		Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::deallocate( p );
+		Base<sizeof(T), PageSize, PageMaxCount>::deallocate( p );
 	}
 	
 	u32 get_object_type( pointer object )
@@ -169,8 +169,8 @@ public:
 		auto f = [&]( )
 		{
 			return
-				( object >= Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::m_data ) &&
-				( object <  Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::m_data + pool_page_size_helper<PageSize>::value * PageMaxCount );
+				( object >= Base<sizeof(T), PageSize, PageMaxCount>::m_data ) &&
+				( object <  Base<sizeof(T), PageSize, PageMaxCount>::m_data + pool_page_size_helper<PageSize>::value * PageMaxCount );
 		};
 
 		ASSERT( f( ) );
@@ -181,25 +181,25 @@ public:
 
 template<template<uptr, uptr, uptr> class Base, uptr PageSize, uptr PageMaxCount, typename T, typename ... TList>
 struct base_dynamic_poolset<Base, PageSize, PageMaxCount, T, TList ...> :
-	Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>,
+	Base<sizeof(T), PageSize, PageMaxCount>,
 	base_dynamic_poolset<Base, PageSize, PageMaxCount, TList ...>
 {
 public:
 	void create( )
 	{
-		Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::create( virtual_mem_allocator( ).reserve( nullptr, pool_page_size_helper<PageSize>::value * PageMaxCount * type_count<T, TList ...>::value ) );
-		base_dynamic_poolset<Base, PageSize, PageMaxCount, TList ...>::create( Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::m_data + pool_page_size_helper<PageSize>::value * PageMaxCount );
+		Base<sizeof(T), PageSize, PageMaxCount>::create( virtual_mem_allocator( ).reserve( nullptr, pool_page_size_helper<PageSize>::value * PageMaxCount * type_count<T, TList ...>::value ) );
+		base_dynamic_poolset<Base, PageSize, PageMaxCount, TList ...>::create( Base<sizeof(T), PageSize, PageMaxCount>::m_data + pool_page_size_helper<PageSize>::value * PageMaxCount );
 	}
 
 	void destroy( )
 	{
-		Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::destroy( );
+		Base<sizeof(T), PageSize, PageMaxCount>::destroy( );
 		base_dynamic_poolset<Base, PageSize, PageMaxCount, TList ...>::destroy( );
 	}
 	
 	void clear( )
 	{
-		Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::clear( );
+		Base<sizeof(T), PageSize, PageMaxCount>::clear( );
 		base_dynamic_poolset<Base, PageSize, PageMaxCount, TList ...>::clear( );
 	}
 	
@@ -218,13 +218,13 @@ public:
 	template<>
 	T* allocate<T>( )
 	{
-		return Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::allocate( sizeof(T) );
+		return Base<sizeof(T), PageSize, PageMaxCount>::allocate( sizeof(T) );
 	}
 
 	template<>
 	void deallocate<T>( T* p )
 	{
-		Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::deallocate( p );
+		Base<sizeof(T), PageSize, PageMaxCount>::deallocate( p );
 	}
 	
 	u32 get_object_type( pointer object )
@@ -233,19 +233,19 @@ public:
 		auto f = [&]( )
 		{
 			return
-				( object >= Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::m_data ) &&
-				( object <  Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::m_data + pool_page_size_helper<PageSize>::value * PageMaxCount * type_count<T, TList ...>::value );
+				( object >= Base<sizeof(T), PageSize, PageMaxCount>::m_data ) &&
+				( object <  Base<sizeof(T), PageSize, PageMaxCount>::m_data + pool_page_size_helper<PageSize>::value * PageMaxCount * type_count<T, TList ...>::value );
 		};
 
 		ASSERT( f( ) );
 #endif // #ifdef DEBUG
-		return (u32)( ( object - Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::m_data ) / ( pool_page_size_helper<PageSize>::value * PageMaxCount ) );
+		return (u32)( ( object - Base<sizeof(T), PageSize, PageMaxCount>::m_data ) / ( pool_page_size_helper<PageSize>::value * PageMaxCount ) );
 	}
 
 protected:
 	void create( pointer memory )
 	{
-		Base<sizeof(T), PageSize / sizeof(T), PageMaxCount>::create( memory );
+		Base<sizeof(T), PageSize, PageMaxCount>::create( memory );
 		base_dynamic_poolset<Base, PageSize, PageMaxCount, TList ...>::create( memory + pool_page_size_helper<PageSize>::value * PageMaxCount );
 	}
 };
