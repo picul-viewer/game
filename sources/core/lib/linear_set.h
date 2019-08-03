d9643c6c4309217f381b488ac720f9784e89b4b9
repeater <none@none>
2019-08-03 14:28:41 +0300

    lib: implement generic hash set

---
+++ sources/core/lib/linear_set.h
@@ -0,0 +1,44 @@
+#ifndef GUARD_CORE_LINEAR_SET_H_INCLUDED
+#define GUARD_CORE_LINEAR_SET_H_INCLUDED
+
+#include <types.h>
+
+namespace lib {
+
+template<typename T, u32 TableSize>
+class linear_set
+{
+public:
+	void create( pointer const in_allocated_memory = nullptr );
+	void destroy( );
+
+	T* find( T const value ) const;
+	bool find_or_insert( T const value, T*& found_value );
+
+	template<typename Functor>
+	T* find_if( u32 const hash, Functor const& f ) const;
+	template<typename Functor, typename InsertValueGenerator>
+	bool find_if_or_insert( u32 const hash, Functor const& f, InsertValueGenerator const& value_gen, T*& found_value );
+
+	T* insert( T const value );
+	void remove( T const value );
+
+public:
+	enum : uptr {
+		key_table_memory_size = TableSize * sizeof(u8),
+		value_table_memory_size = TableSize * sizeof(T),
+
+		memory_size = key_table_memory_size + value_table_memory_size
+	};
+
+private:
+	u8* m_key_table;
+	T* m_value_table;
+
+};
+
+} // namespace lib
+
+#include "linear_set_inline.h"
+
+#endif // #ifndef GUARD_CORE_LINEAR_SET_H_INCLUDED
---
+++ sources/core/lib/linear_set_inline.h
@@ -0,0 +1,253 @@
+#ifndef GUARD_CORE_LINEAR_INLINE_SET_H_INCLUDED
+#define GUARD_CORE_LINEAR_INLINE_SET_H_INCLUDED
+
+#include <macros.h>
+#include <lib/algorithms.h>
+#include <lib/allocator.h>
+#include <lib/hash.h>
+
+namespace lib {
+
+template<typename T, u32 TableSize>
+void linear_set<T, TableSize>::create( pointer const in_allocated_memory )
+{
+	static_assert( key_table_memory_size % Memory_Page_Size == 0, "invalid table size" );
+	static_assert( value_table_memory_size % Memory_Page_Size == 0, "invalid table size" );
+
+	pointer const memory =
+		( in_allocated_memory == nullptr ) ?
+		virtual_allocator( ).allocate( memory_size ) :
+		in_allocated_memory;
+
+	m_key_table = memory;
+	m_value_table = memory + key_table_memory_size;
+}
+
+template<typename T, u32 TableSize>
+void linear_set<T, TableSize>::destroy( )
+{
+	virtual_allocator( ).deallocate( m_key_table );
+}
+
+template<typename T, u32 TableSize>
+T* linear_set<T, TableSize>::find( T const value ) const
+{
+	return find_if( hash( value ), [&value]( T const& v )
+	{
+		return v == value;
+	} );
+}
+
+template<typename T, u32 TableSize>
+bool linear_set<T, TableSize>::find_or_insert( T const value, T*& found_value )
+{
+	return find_if_or_insert(
+		hash( value ),
+		[&value]( T const& v )
+		{
+			return v == value;
+		},
+		[&value]( )
+		{
+			return lib::move( value );
+		},
+		found_value
+	);
+}
+
+template<typename T, u32 TableSize>
+template<typename Functor>
+T* linear_set<T, TableSize>::find_if( u32 const hash, Functor const& f ) const
+{
+	u32 const max_distance = ( TableSize < 256 ) ? TableSize : 255;
+
+	u32 const start_index = hash % TableSize;
+	u32 index = start_index;
+
+	for ( u8 i = 1; ; ++i )
+	{
+		u8 const current_key = m_key_table[index];
+
+		if ( i == current_key )
+		{
+			T const current_value = m_value_table[index];
+
+			if ( f( current_value ) )
+			{
+				return m_value_table + index;
+			}
+		}
+
+		if ( i > current_key )
+			break;
+
+		index = ( index + 1 ) % TableSize;
+
+		ASSERT( i != max_distance, "set is full" );
+	}
+
+	return nullptr;
+}
+
+template<typename T, u32 TableSize>
+template<typename Functor, typename InsertValueGenerator>
+bool linear_set<T, TableSize>::find_if_or_insert( u32 const hash, Functor const& f, InsertValueGenerator const& value_gen, T*& found_value )
+{
+	u32 const max_distance = ( TableSize < 256 ) ? TableSize : 255;
+
+	u32 const start_index = hash % TableSize;
+	u32 index = start_index;
+
+	u8 current_key;
+	u8 i;
+
+	for ( i = 1; ; ++i )
+	{
+		current_key = m_key_table[index];
+
+		if ( i == current_key )
+		{
+			T const current_value = m_value_table[index];
+
+			if ( f( current_value ) )
+			{
+				found_value = m_value_table + index;
+				return true;
+			}
+		}
+
+		if ( i > current_key )
+			break;
+
+		index = ( index + 1 ) % TableSize;
+
+		ASSERT( i != max_distance, "set is full" );
+	}
+	
+	u8 key_to_insert = current_key;
+	T value_to_insert = m_value_table[index];
+
+	m_key_table[index] = i;
+	m_value_table[index] = value_gen( );
+
+	found_value = m_value_table + index;
+
+	while ( key_to_insert != 0 )
+	{
+		index = ( index + 1 ) % TableSize;
+
+		current_key = m_key_table[index];
+
+		ASSERT( ( key_to_insert & 0xFF ) != 0xFF, "unable to insert ( too long offset )" );
+		++key_to_insert;
+
+		if ( current_key < key_to_insert )
+		{
+			lib::swap( m_key_table[index], key_to_insert );
+			lib::swap( m_value_table[index], value_to_insert );
+		}
+	}
+
+	return false;
+}
+
+template<typename T, u32 TableSize>
+T* linear_set<T, TableSize>::insert( T const value )
+{
+	u32 const max_distance = ( TableSize < 256 ) ? TableSize : 255;
+
+	u32 const start_index = hash( value ) % TableSize;
+	u32 index = start_index;
+
+	u8 current_key;
+	u8 i;
+
+	for ( i = 1; ; ++i )
+	{
+		current_key = m_key_table[index];
+
+		if ( i > current_key )
+			break;
+
+		index = ( index + 1 ) % TableSize;
+
+		ASSERT( i != max_distance, "set is full" );
+	}
+	
+	u8 key_to_insert = current_key;
+	T value_to_insert = m_value_table[index];
+
+	m_key_table[index] = i;
+	m_value_table[index] = value;
+
+	T* const result = m_value_table + index;
+
+	while ( key_to_insert != 0 )
+	{
+		index = ( index + 1 ) % TableSize;
+
+		current_key = m_key_table[index];
+
+		ASSERT( ( key_to_insert & 0xFF ) != 0xFF, "unable to insert ( too long offset )" );
+		++key_to_insert;
+
+		if ( current_key < key_to_insert )
+		{
+			lib::swap( m_key_table[index], key_to_insert );
+			lib::swap( m_value_table[index], value_to_insert );
+		}
+	}
+
+	return result;
+}
+
+template<typename T, u32 TableSize>
+void linear_set<T, TableSize>::remove( T const value )
+{
+	u32 const start_index = hash( value ) % TableSize;
+	u32 index = start_index;
+
+	u8 current_key;
+	T current_value;
+	u8 i;
+
+	for ( i = 1; ; ++i )
+	{
+		current_key = m_key_table[index];
+
+		if ( i == current_key )
+		{
+			T const current_value = m_value_table[index];
+
+			if ( value == current_value )
+				break;
+		}
+
+		ASSERT( i <= current_key, "element does not exist" );
+
+		index = ( index + 1 ) % TableSize;
+	}
+
+	while ( true )
+	{
+		u32 const next_index = ( index + 1 ) % TableSize;
+
+		current_key = m_key_table[next_index];
+		current_value = m_value_table[next_index];
+
+		if ( current_key <= 1 )
+			break;
+
+		m_key_table[index] = current_key - 1;
+		m_value_table[index] = current_value;
+
+		index = next_index;
+	}
+
+	m_key_table[index] = 0;
+	m_value_table[index] = 0;
+}
+
+} // namespace lib
+
+#endif // #ifndef GUARD_CORE_LINEAR_INLINE_SET_H_INCLUDED
