b462d3d78dafc7f0b612f5e1e636a6fe9ddfa1b4
picul <none@none>
2020-04-19 14:46:41 +0300

    remove redudant shader files

--- resources/sources/shaders/old/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef __shader_common_h_included_
-#define __shader_common_h_included_
-
-float4 transform_position( float3 position, float4x4 transform )
-{
-	return mul( transform, float4( position.xyz, 1.0f ) );
-}
-
-#endif // #ifndef __shader_common_h_included_
