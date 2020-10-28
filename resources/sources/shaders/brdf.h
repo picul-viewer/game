a733803bf4f2464822271c8a63f1065515de0f8d
picul <none@none>
2020-10-28 02:35:30 +0200

    render: optimize BRDF calculation

--- resources/sources/shaders/brdf.h
+++ resources/sources/shaders/brdf.h
@@ -55,10 +55,21 @@ float3 brdf(
 	
 	const float3 diff = lambertian_diffuse( diffuse_albedo );
 	
-	const float d = d_ggx( roughness, n_dot_h );
+	/*const float d = d_ggx( roughness, n_dot_h );
 	const float3 f = f_schlick( specular_albedo, io_dot_h );
 	const float g = g_smith_disney( roughness, clamped_i_dot_n, clamped_o_dot_n );
-	const float3 spec = d * f * g / ( 4.0f * clamped_i_dot_n * clamped_o_dot_n );
+	const float3 spec = d * f * g / ( 4.0f * clamped_i_dot_n * clamped_o_dot_n );*/
+
+	// Optimized version.
+	const float alpha = roughness * roughness;
+	const float alpha2 = alpha * alpha;
+	const float ggx_denom = n_dot_h * n_dot_h * ( alpha2 - 1.0f ) + 1.0f;
+	const float disney_rough = ( roughness + 1.0f ) * 0.5f;
+	const float k = 0.5f * disney_rough * disney_rough;
+	const float3 f = f_schlick( specular_albedo, io_dot_h );
+	const float numerator = 0.25f * c_inv_pi * alpha2;
+	const float denominator = max( ggx_denom * ggx_denom, c_eps ) * ( k + clamped_i_dot_n * ( 1.0f - k ) ) * ( k + clamped_o_dot_n * ( 1.0f - k ) );
+	const float3 spec = f * numerator / denominator;
 	
 	return diff + spec;
 }
